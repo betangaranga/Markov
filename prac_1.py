2c6f7fbd6b59f28a292a0469b92dc09717e72f1d
Angel <albertoangelaguilera@gmail.com>
2018-02-20 22:43:15 -0600

    Se soluciona problema de compatibilidad den Linux

--- prac_1.py
+++ prac_1.py
@@ -1,6 +1,6 @@
 import unidecode,string,math
 ABC=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',' '];
-file = open("dante.txt","r")
+file = open("dante.txt","r",encoding = "ISO-8859-1")
 repeticiones=[]
 probabilidades=[]
 entropias=[]
@@ -13,13 +13,15 @@ texto = ''.join([i for i in texto_puntuacion if not i.isdigit()])
 for letra in ABC:
     repeticiones.append(texto.count(letra))
     index=ABC.index(letra)
-    probabilidades.append(repeticiones[index]/len(texto))
-print(repeticiones)#Repeticiones contiene ahora el num de repeticiones por caracter en el texto
-print(sum(probabilidades))
+    probabilidades.append(repeticiones[index]/len(texto))#Repeticiones contiene ahora el num de repeticiones por caracter en el texto
 for probabilidad in probabilidades:
     try:
         entropias.append(math.log((1/probabilidad),2)*probabilidad)
     except ZeroDivisionError:
         entropias.append(0)
 
-print(sum(entropias))
+print ("Repeticiones : ",dict(zip(ABC,repeticiones)))
+print ("Probabilidades : ",dict(zip(ABC,probabilidades)))
+print("Suma de probabilidades : ",sum(probabilidades))
+print("Entropia total sin memoria : ",sum(entropias))
+print("10 probabilidades mas altas : ",sorted(probabilidades,reverse=True))
